e375559d51e79fb638e45e731f5492456c4d4ad9
Yohanan Boy <yohanan.santos@aluno.cefet-rj.br>
2026-03-20 19:20:46 +0000

    Segundo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,7 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
 			<h1>{text}</h1>
 			<Menu/>
-			<button onclick="teste(text)">Mudar o h1</button>
 			</div>
   );
 }
-
-function teste(&text){
-	text = "mudou";
-}
-
 
