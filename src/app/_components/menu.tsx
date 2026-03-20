f79e1de8bbb19607cba5d66bcf12b4e81599d7dd
Yohanan Boy <yohanan.santos@aluno.cefet-rj.br>
2026-03-20 20:51:44 +0000

    feat: implementando next t3 e usando useState

--- src/app/_components/menu.tsx
+++
@@ -1,9 +0,0 @@
-	export default function Menu(){
-		return <nav>
-			<ul>
-				<li><a href="#">Pagina Inícial</a></li>
-				<li><a href="#">Contato</a></li>
-				<li><a href="https://yohananboy.github.io/loudnews">LoudNews</a></li>
-			</ul>
-		</nav>
-	}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,39 @@
-import Image from "next/image";
-import Menu from "./_components/menu.tsx";
+"use client";
+import Link from "next/link";
+import {useState} from "react";
 
-export default function Home() {
-	const text = "Site top top top";
-	
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<h1>{text}</h1>
-			<Menu/>
-			</div>
+export default function HomePage() {
+  const [num, setNum] = useState(0);
+	const [text, setText] = useState("");
+
+	return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Exercícios
+					</h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+					<div>
+						<h2 className="text-3xl">Soma</h2>
+						<br/>
+						{num}
+						<br/>
+						<button onClick={() => {
+							setNum(num + 1);
+						}}>+1</button>
+					</div>
+					
+					<div>
+						<h2 className="text-3xl">Input</h2>
+						<br/>
+						{text}
+						<br/>
+						<input className="bg-blue-200" onChange={(item)=>{
+							setText(item.target.value);
+						}}/>
+					</div>
+				</div>
+      </div>
+    </main>
   );
 }
-
