c4af49b87baeb7f4dbeb88fc42aa0ebdf43d47cc
Yohanan <14911025786@cefet-rj.br>
2026-03-26 13:25:09 -0300

    fix: movendo layout.tsx para pasta raiz

--- src/app/usestate/page.tsx
+++ src/app/usestate/page.tsx
@@ -1,38 +1,45 @@
 "use client";
 import Link from "next/link";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function HomePage() {
   const [num, setNum] = useState(0);
-	const [text, setText] = useState("");
+  const [text, setText] = useState("");
 
-	return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Exercícios
-					</h1>
+        </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-					<div>
-						<h2 className="text-3xl">Soma</h2>
-						<br/>
-						{num}
-						<br/>
-						<button onClick={() => {
-							setNum(num + 1);
-						}}>+1</button>
-					</div>
-					
-					<div>
-						<h2 className="text-3xl">Input</h2>
-						<br/>
-						{text}
-						<br/>
-						<input className="bg-blue-200" onChange={(item)=>{
-							setText(item.target.value);
-						}}/>
-					</div>
-				</div>
+          <div>
+            <h2 className="text-3xl">Soma</h2>
+            <br />
+            {num}
+            <br />
+            <button
+              onClick={() => {
+                setNum(num + 1);
+              }}
+            >
+              +1
+            </button>
+          </div>
+
+          <div>
+            <h2 className="text-3xl">Input</h2>
+            <br />
+            {text}
+            <br />
+            <input
+              className="bg-blue-200"
+              onChange={(item) => {
+                setText(item.target.value);
+              }}
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
